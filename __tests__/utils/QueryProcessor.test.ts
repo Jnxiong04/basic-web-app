e84971eeb915358f12c622f78aa80192ad2c02a5
jnxiong <janiexio@andrew.cmu.edu>
2023-09-19 15:54:11 -0400

    added addition query

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -34,4 +34,8 @@ describe("QueryProcessor", () => {
           ));
     });
 
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
 });
